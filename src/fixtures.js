f53657c2f0b955a5fe62c833940c34c4487b6fb2
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-01-15 22:00:33 +0200

    add fixture to comment

--- src/fixtures.js
+++ src/fixtures.js
@@ -2,7 +2,14 @@ export default [
     {
         title: 'My first article',
         text: 'Lorem Ipsum',
-        id: 1
+        id: 1,
+        comments: [
+            {
+                id: 1,
+                text: 'some comment',
+                author: 'roma'
+            }
+        ]
     },{
         title: 'Other Article',
         text: 'Some other text',
