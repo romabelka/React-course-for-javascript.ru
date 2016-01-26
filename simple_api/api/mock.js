9937c0c3f8d7518bca7af475180beefbf189033c
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-01-26 16:14:38 +0200

    api: add mock comments

--- simple_api/api/mock.js
+++ simple_api/api/mock.js
@@ -51,6 +51,174 @@ var comments = [
         timeStamp: new Date(),
         article: 1,
         user: 'roma'
+    },
+    {
+        id: 4,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 5,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 6,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 7,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 8,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 9,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 10,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 11,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 12,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 13,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 14,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 15,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 16,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 17,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 18,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 19,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 20,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 21,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 22,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 23,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 24,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
+    },
+    {
+        id: 25,
+        text: 'first comment',
+        timeStamp: new Date(),
+        user: 'oleg',
+        article: 2
+    },
+    {
+        id: 26,
+        text: 'second comment',
+        timeStamp: new Date(),
+        user: 'roma',
+        article: 1
+    },
+    {
+        id: 27,
+        text: 'third comment',
+        timeStamp: new Date(),
+        article: 1,
+        user: 'roma'
     }
 ];
 
