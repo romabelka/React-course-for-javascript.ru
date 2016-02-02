5358f8cee5cba99470b5d9ad3a02e7cc95af2ba2
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-02 21:46:22 +0200

    add simple DevTools for reducers

--- src/store/index.js
+++ src/store/index.js
@@ -8,4 +8,13 @@ const store = compose(
     DevToolsContainer.instrument()
 )(createStore)(reducer)
 
+if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducer', () => {
+        const nextRootReducer = require('../reducer')
+        store.replaceReducer(nextRootReducer)
+    })
+}
+
+
 export default store
