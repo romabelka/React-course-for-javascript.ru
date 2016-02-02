8828b0af4d21f4da17f3d46d3422b31159dc49fb
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-02 21:21:51 +0200

    add simple redux cycle

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -1,23 +1,31 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { increment } from '../AC'
 
 class Counter extends Component {
     static propTypes = {
-        counter: PropTypes.number
+        counter: PropTypes.number,
+        increment: PropTypes.func
     };
 
     render() {
         return (
             <div>
                 {this.props.counter}
+                <a href = "#" onClick = {this.handleClick}>increment</a>
             </div>
         )
     }
+
+    handleClick = (ev) => {
+        ev.preventDefault()
+        this.props.increment()
+    };
 }
 
 export default connect((state) => {
     const { counter } = state
-    return {
-        counter
-    }
+    return { counter }
+}, {
+    increment
 })(Counter)
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Provider } from 'react-redux'
+import Counter from './Counter'
 
 class Main extends Component {
     static propTypes = {
@@ -9,9 +10,7 @@ class Main extends Component {
     render() {
         return (
             <Provider store = {this.props.store}>
-                <div>
-
-                </div>
+                <Counter />
             </Provider>
         )
     }
