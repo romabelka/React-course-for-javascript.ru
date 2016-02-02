2a1dc8863f05f0b0cace7a1adf46c734ac46ee9d
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-02 17:02:09 +0200

    add selectbox with style-loader

--- src/components/AllComments.js
+++ src/components/AllComments.js
@@ -1,11 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 import { comments } from '../stores'
+import Select from 'react-select'
+import { uniq } from 'lodash'
 
+require('react-select/dist/react-select.min.css')
 class AllComments extends Component {
     constructor(...args) {
         super(...args)
         this.state = {
-            comments: comments.getOrLoadAll()
+            comments: comments.getOrLoadAll(),
+            user: undefined
         }
     }
 
@@ -22,15 +26,37 @@ class AllComments extends Component {
     }
 
     render() {
-        const { comments } = this.state
+        const { comments, user } = this.state
         if (!comments || !comments.length) return <h3>Loading...</h3>
-        const commetList = comments.map((comment) => {
-            return <li key = {comment.id}>{comment.text}</li>
+        const users = uniq(comments.map(comment => comment.user))
+        const options = users.map((name) => {
+            return {
+                label: name,
+                value: name
+            }
         })
+
+        const commetList = comments
+            .filter((comment) => !user || comment.user == user)
+            .map((comment) => {
+                return <li key = {comment.id}>{comment.text} <b> by {comment.user}</b></li>
+            })
         return (
-            <ul>{commetList}</ul>
+            <div>
+                <Select
+                    options = {options}
+                    multi = {false}
+                    onChange = {this.selectUser}
+                    value = {user}
+                />
+                <ul>{commetList}</ul>
+            </div>
         )
     }
+
+    selectUser = (user) => {
+        this.setState({ user })
+    };
 
     changeState = (props) => {
         this.setState({
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,6 +21,9 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
     }]
   }
 };
